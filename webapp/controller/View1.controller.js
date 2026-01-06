985afb2007608a0bf2ac75d844f9f7ab271a9e3e
Victor Fibechola Okpanachi <victor-fibechola.okpanachi@hsrw.org>
2026-01-06 00:58:23 +0000

    get and set values dynamically changing ui

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,8 +4,20 @@ sap.ui.define([
   "use strict";
 
   return Controller.extend("com.clientname.zsapui5.controller.View1", {
+
+    onInit(){
+      this.initialWelcomeTxt = this.getView().byId("idWelcomTxt").getText();   //controller variable
+    },
+
     onNavToView2: function () {
       this.getOwnerComponent().getRouter().navTo("RouteView2");     //navTo takes only the routename
+    },
+    onPressSubmit: function(){
+      var name = this.getView().byId("idInpName").getValue();   //get the input value
+      var welcomeMsg = name + "-" + this.initialWelcomeTxt  //get the text ui element
+      
+      //now set the concantenated message back to the ui element
+      this.getView().byId("idWelcomTxt").setText(welcomeMsg);
     }
   });
 });
